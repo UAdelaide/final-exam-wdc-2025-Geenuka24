e1b07d556b2fd01532b02088efaaadacded73977
geenuka24 <avingeenu@gmail.com>
2025-06-20 10:13:34 +0000

    [automatic commit] 2025-06-20 10:13:33 UTC 120.20.38.47

--- part1/app.js
+++ part1/app.js
@@ -66,7 +66,8 @@ let db;
                 await db.execute(`
                     INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes,location,status)
                     VALUES
-                    ((SELECT dog_id FROM Dogs WHERE name= 'Max' AND owner_id =SELECT user_id FROM Users WHERE username='alice123')),`)
+                    ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),'2025-06-10 08:00:00', 30, 'Parklands','open',
+                    `)
             }
     }
 });
