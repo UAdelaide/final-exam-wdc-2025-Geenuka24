5afdaf707b9440f56bb63c502320377e92fd59a8
geenuka24 <avingeenu@gmail.com>
2025-06-20 11:03:02 +0000

    [automatic commit] 2025-06-20 11:03:02 UTC 120.20.38.47

--- part1/app.js
+++ part1/app.js
@@ -128,7 +128,7 @@ app.get('api/walkers/summary', async(req,res)=>{
             AVG(WalkRating.rating) average_rating,
             SUM(CASE WHEN WalkRequest.status='completed' THEN 1 ELSE 0 END) completed_walks
             FROM Users
-            LEFT JOIN WalkApplications ON Users.userid=WalkAP`)
+            LEFT JOIN WalkApplications ON Users.userid=WalkApplications.walker_id`)
     }
 })
 module.exports = app;
