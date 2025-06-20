d94b56bd5180a492f497d14070309aaf1adb004b
geenuka24 <avingeenu@gmail.com>
2025-06-20 09:37:58 +0000

    [automatic commit] 2025-06-20 09:37:58 UTC 120.20.38.47

--- part1/app.js
+++ part1/app.js
@@ -49,7 +49,7 @@ let db;
         }
 
         // Dogs table
-        // var [dogRows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
+        var [dogRows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
         if (dogRows[0].count === 0) {
             await db.execute(`
                 INSERT INTO Dogs(owner_id, name, size)
