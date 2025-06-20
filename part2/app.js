73ad66a176e7269c5c13e1cee368ed8d7da782cb
geenuka24 <avingeenu@gmail.com>
2025-06-20 23:16:47 +0000

    [automatic commit] 2025-06-20 23:16:47 UTC 120.20.216.59

--- part2/app.js
+++ part2/app.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(session({
     secret:'';
-    
+    resave:false;
+    saveUninitialised
 }))
 app.use(express.static(path.join(__dirname, '/public')));
 
