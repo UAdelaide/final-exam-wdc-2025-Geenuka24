60c1b5d4656728b4d98c499e6fc344a27e11e396
geenuka24 <avingeenu@gmail.com>
2025-06-20 10:08:56 +0000

    [automatic commit] 2025-06-20 10:08:56 UTC 120.20.38.47

--- part1/app.js
+++ part1/app.js
@@ -66,7 +66,7 @@ let db;
                 await db.execute(`
                     INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes,location,status)
                     VALUES
-                    (())`)
+                    ((SELECT ))`)
             }
     }
 });
