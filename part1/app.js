ed7120e6dacf3a2edfb2093484c85666d9541f42
geenuka24 <avingeenu@gmail.com>
2025-06-20 09:22:42 +0000

    [automatic commit] 2025-06-20 09:22:41 UTC 120.20.38.47

--- part1/app.js
+++ part1/app.js
@@ -39,7 +39,8 @@ let db;
 
         var [userRows]= await db.execute('SELECT COUNT(*) AS count FROM Users');
         if(userRows[0].count ===0){
-            await db.execute(`INSERT INTO Users (username,email,password_hash, role)`)
+            await db.execute(`INSERT INTO Users (username,email,password_hash, role)
+                VALUES ()`)
         }
     }
 });
