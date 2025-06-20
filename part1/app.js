f01873490c07f756ae278dcd4dc9f5175442a608
geenuka24 <avingeenu@gmail.com>
2025-06-20 09:54:23 +0000

    [automatic commit] 2025-06-20 09:54:23 UTC 120.20.38.47

--- part1/app.js
+++ part1/app.js
@@ -62,7 +62,9 @@ let db;
             }
             // WalkRequests
             const [requestRows]= await db.execute('SELECT COUNT(*) AS count FROM Dogs');
-            if(requestRows[0])
+            if(requestRows[0].count ===0){
+                await db.exec
+            }
     }
 });
 module.exports = app;
