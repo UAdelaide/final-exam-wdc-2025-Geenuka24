3b800d30bbd7ca3983dc63655268821c2b2b38de
geenuka24 <avingeenu@gmail.com>
2025-06-20 23:23:24 +0000

    [automatic commit] 2025-06-20 23:23:24 UTC 120.20.216.59

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -51,6 +51,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
     const user=rows[0];
+    req.session.user={id: user.user_id, username}
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
