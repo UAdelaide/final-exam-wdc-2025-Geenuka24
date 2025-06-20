2031441a6bef0daf2b0360bc14fa3b20dc3e392c
geenuka24 <avingeenu@gmail.com>
2025-06-20 11:16:19 +0000

    [automatic commit] 2025-06-20 11:16:19 UTC 120.20.38.47

--- part1/app.js
+++ part1/app.js
@@ -67,7 +67,7 @@ let db;
                     INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes,location,status)
                     VALUES
                     ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),'2025-06-10 08:00:00', 30, 'Parklands','open'),
-                    ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')),'2025-06-10 09:30:00',45, 'Beachside Ave','accepted',)
+                    ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')),'2025-06-10 09:30:00',45, 'Beachside Ave','accepted'),
                     ((SELECT dog_id FROM Dogs WHERE name = 'Shaq' AND owner_id = (SELECT user_id FROM Users WHERE username = 'kobe824')),'2025-06-11 07:30:00',60,'Beachside Ave', 'open'),
                     ((SELECT dog_id FROM Dogs WHERE name = 'Mamba' AND owner_id = (SELECT user_id FROM Users WHERE username = 'kobe824')), '2025-06-12 14:00:00', 30,'Parklands', 'completed'),
                     ((SELECT dog_id FROM Dogs WHERE name = 'foo' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-13 16:00:00', 45, 'Beachside Ave', 'open')
