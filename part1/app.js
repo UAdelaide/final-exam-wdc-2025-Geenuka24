fab3a0b233f412aaf05213ac5da96abc50201de7
geenuka24 <avingeenu@gmail.com>
2025-06-20 10:59:27 +0000

    [automatic commit] 2025-06-20 10:59:27 UTC 120.20.38.47

--- part1/app.js
+++ part1/app.js
@@ -121,7 +121,7 @@ app.get('/api/walkrequests/open', async(req,res)=> {
 // API WALKERS SUMMARY
 app.get('api/walkers/summary', async(req,res)=>{
     try{
-        
+        var[rows]
     }
 })
 module.exports = app;
