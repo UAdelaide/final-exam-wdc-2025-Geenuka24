091ec58a9b5d12316a7ff5019d3854d1d8d49416
geenuka24 <avingeenu@gmail.com>
2025-06-21 06:07:16 +0000

    [automatic commit] 2025-06-21 06:07:16 UTC 159.196.59.17

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -70,7 +70,7 @@ router.post('/logout', async (req,res) => {
 });
 
 // Post on getting owners dogs
-router.get('/dogs', async(req,res) => {
+router.get('/dogs/:ownerId', async(req,res) => {
   try{
     const[rows]=await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [req.query.owner_id]);
     res.json(rows);
