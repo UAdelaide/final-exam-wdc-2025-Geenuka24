813a084056973c64fde3a90523090890780684c0
geenuka24 <avingeenu@gmail.com>
2025-06-21 03:38:13 +0000

    [automatic commit] 2025-06-21 03:38:13 UTC 159.196.59.17

--- part2/app.js
+++ part2/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+const app = express();
 const session = require('express-session');
 
-const app = express();
 
 // Middleware
 app.use(express.json());
